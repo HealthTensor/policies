4fb1e5bfc11d3317a0aa61bce47aca32650f8a45
Thomas Moulia <jtmoulia@gmail.com>
2017-06-12 15:22:12 -0700

    Add script and instructions for compliance checks
    Summary:
    
    This commit adds instructions to the training for securing a HIPAA compliant
    workstation. `bin/compliance.sh` checks some configurations, and reports
    compliance
    
    Test plan:
    
    Run the compliance shell script:
    
    ```
    ./bin/compliance.sh
    ```

---
+++ bin/compliance.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0;0m'
+FAILED=
+AUTOLOCK_SECS=600
+SSH_KEY=~/.ssh/id_rsa
+
+function success {
+    printf "${GREEN}$@${NC}\n"
+}
+
+function fail {
+    printf "${RED}$@${NC}\n"
+    FAILED=true
+}
+
+if [[ "$(grep "Proc-Type: 4,ENCRYPTED" ${SSH_KEY})" ]]; then
+    success "SSH key is encrypted (${SSH_KEY})."
+else
+    fail "SSH key NOT encrypted: ${SSH_KEY}!!!"
+fi
+ssh_key_size="$(ssh-keygen -l -f "${SSH_KEY}" | cut -f1 -d' ')"
+if [[ "$ssh_key_size" -ge 2048 ]]; then
+    success "SSH key size 2048 bits or greater (${ssh_key_size})."
+else
+    fail "SSH key size less than 2048 bits: ${ssh_key_size}!!!"
+fi
+ssh_key_type="$(ssh-keygen -l -f "${SSH_KEY}" | cut -f4 -d' ')"
+if [[ "$ssh_key_type" == "(RSA)" ]]; then
+    success "SSH key is RSA."
+else
+    fail "SSH key isn't RSA: ${ssh_key_type}!!!"
+fi
+
+case "$(uname)" in
+    Darwin)
+        if [[ "$(fdesetup status | grep "FileVault")" == "FileVault is On." ]]; then
+            success "FileVault is on."
+        else
+            fail "FileVault not on!!! Fix: https://support.apple.com/en-gb/HT204837"
+        fi
+        if [[ "$(defaults read com.apple.screensaver askForPassword)" == "1" ]]; then
+            success "Screensaver autolock enabled."
+        else
+            fail "Screensaver autolock disabled!!!"
+        fi
+        autolock="$(defaults read com.apple.screensaver askForPasswordDelay)"
+        if [[ $autolock -le $AUTOLOCK_SECS ]]; then
+            success "Autolocks in less than ${AUTOLOCK_SECS}s (${autolock}s)."
+        else
+            fail "Autolock takes longer than ${AUTOLOCK_SECS}s (${autolock}s)!"
+        fi
+        ;;
+    *)
+        # fail "Unrecognized OS: $(uname)"
+        ;;
+esac
+
+echo "=========================="
+
+if [[ "$FAILED" ]]; then
+    echo "COMPLIANCE CHECK FAILED!!!"
+    echo "Fix above issues before pulling PHI."
+else
+    echo "Public SSH Key (${SSH_KEY}.pub):"
+    echo "$(ssh-keygen -l -f "${SSH_KEY}.pub")"
+    cat "${SSH_KEY}.pub"
+    echo "Success! Send this output to scalar@healthtensor.com"
+fi
---
+++ source/checklist.md
@@ -0,0 +1,6 @@
+# HIPAA Compliance Checklist
+
+    - [ ] Complete the annual [HealthTensor training](http://policies.healthtensor.com/training)
+    - [ ] Ensure `bin/compliance.sh` passes, report output. ([repo](https://github.com/HealthTensor/policies))
+    - [ ] Get bastion key.
+    - [ ] Create an SSH tunnel to the database.
